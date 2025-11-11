7a4d0b4f709312fbccae66f08d73774ba4f90c31
Daniel Marquez <daniel.sepulveda@correounivalle.edu.co>
2025-11-11 14:36:38 +0000

    agregando importaciones en el main

--- app/src/main/java/com/example/miniproyecto1/view/MainActivity.kt
+++ app/src/main/java/com/example/miniproyecto1/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.miniproyecto1.view
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import com.example.miniproyecto1.R
 
